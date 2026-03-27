8ecf8b6f13c68670ec06d7127bfb75b4c7b31411
giddygamer505 <xvvsti@gmail.com>
2026-03-27 18:37:58 +0700

    Update Admin page

--- election/admin.py
+++ election/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Candidate
+from .models import Candidate,Voter
 
-admin.site.register(Candidate)
+@admin.register(Voter)
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ('voter_id', 'has_voted')
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'votes')
