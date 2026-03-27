cfd817b57aece65c144961d0e068c7af55cd8489
giddygamer505 <xvvsti@gmail.com>
2026-03-27 17:27:47 +0700

    UT Model

--- election/models.py
+++ election/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
--- election/tests.py
+++ election/tests.py
@@ -1,6 +1,22 @@
 from django.test import TestCase
+from .models import Candidate
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+class CandidateModelTest(TestCase):
+    def test_candidate_creation(self):
+        candidate = Candidate.objects.create(name="Jesus Christ", votes=10)
+        
+        self.assertEqual(candidate.name, "Jesus Christ")
+        self.assertEqual(candidate.votes, 10)
+        
+    def test_default_votes(self):
+        candidate = Candidate.objects.create(name="Jesus Christ")
+        self.assertEqual(candidate.votes, 0)
+
+    def test_model_exists_in_db(self):
+        Candidate.objects.create(name="Test User")
+        self.assertEqual(Candidate.objects.count(), 1)
