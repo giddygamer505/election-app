c870484b31d3339bbe4c4be3933b303248319617
giddygamer505 <xvvsti@gmail.com>
2026-03-27 18:33:55 +0700

    Voter model

--- election/models.py
+++ election/models.py
@@ -6,3 +6,10 @@ class Candidate(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Voter(models.Model):
+    voter_id = models.CharField(max_length=20, unique=True)
+    has_voted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.voter_id
