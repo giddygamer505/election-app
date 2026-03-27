1a536c44d779317565083704e8fdbb351f0e2b9c
giddygamer505 <xvvsti@gmail.com>
2026-03-27 18:02:36 +0700

    Vote update

--- election/views.py
+++ election/views.py
@@ -4,6 +4,11 @@ from .models import Candidate
 # Create your views here.
 def home_view(request):
     if request.method == 'POST':
-        return redirect('home')
+        candidate_id = request.POST.get('candidate_id')
+        candidate = Candidate.objects.get(id=candidate_id)
+        candidate.votes += 1
+        candidate.save()
+        return redirect('.') 
+    
     candidates = Candidate.objects.all()
     return render(request,'home.html',{'candidates' : candidates})
